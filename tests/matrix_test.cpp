28c9426d6a15d09e10452bd87d1c28b5b5a9c653
Vincent Gay <glurbi@gmail.com>
2015-08-27 10:44:04 +0200

    vector add test

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -1,10 +1,15 @@
 #include <gtest/gtest.h>
-#include <iostream>
+
+#include <matrix.hpp>
 
 using namespace std;
 
-TEST(test_case_name, test_name)
+TEST(matrix, add_two_vectors)
 {
-    cout << "mytest!" << endl;
-    system("pause"); 
+    auto v1 = yae::vector3f{ 1.0f, 2.0f, 3.0f };
+    auto v2 = yae::vector3f{ 2.0f, 3.0f, 4.0f };
+    auto v3 = v1 + v2;
+    ASSERT_FLOAT_EQ(3.0f, v3.x());
+    ASSERT_FLOAT_EQ(5.0f, v3.y());
+    ASSERT_FLOAT_EQ(7.0f, v3.z());
 }
